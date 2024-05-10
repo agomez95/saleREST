3b0246cd1b622d57a34a8f5c0b484c56093baed7
Adrian Gomez V <sandmancity14@gmail.com>
2024-05-10 18:25:48 -0500

    [sales] refactorized

--- src/utils/moduleError.js
+++ src/utils/moduleError.js
@@ -4,7 +4,59 @@ const { CustomError, BadRequestError, UnauthorizedError, ForbiddendError, NotFou
 
 class moduleErrorHandler {
     static handleError(error) {
-        if (error.code === '23505') {
+        switch(error.code) {
+            case '23505':
+                throw new ConflictError(409, 'ERROR: DUPLICATE KEY VALUE', error.code);
+                break;
+            case '23503':
+                throw new ConflictError(409, 'ERROR: FOREIGN KEY VIOLATION', error.code);
+                break;
+            case '42703':
+                throw new BadRequestError(400, 'ERROR: UNDEFINED COLUMN', error.code);
+                break;
+            case '42P01':
+                throw new NotFoundError(404, 'ERROR: UNDEFINED TABLE', error.code);
+                break;
+            case '08001':
+                throw new ServiceUnavailableError(503, 'ERROR: CONNECTION FAILURE', error.code);
+                break;
+            case '53300':
+                throw new GatewayTimeoutError(504, 'ERROR: CONNECTION TIMED OUT', error.code);
+                break;
+            case '28000':
+                throw new InternalServerError(500, 'ERROR: INVALID DATABASE AUTHORIZATION CREDENTIALS', error.code);
+                break;
+            case '3D000':
+                throw new NotFoundError(404, 'ERROR: ' + error.message.toUpperCase(), error.code);
+                break;
+            case '42883':
+                throw new NotFoundError(409, `ERROR: FUNCTION DOESN'T EXIST`, error.code);
+                break;
+            case '08006':
+                let message = (error.message) ? error.message : 'ERROR: AUTHENTICATION ERROR, CREDENTIALS FAILED';
+                throw new UnauthorizedError(401, message, error.code);
+                break;
+            case '08007':
+                throw new InternalServerError(500, 'ERROR: CONNECTION FAILED, ' + error.message.toUpperCase(), error.code);
+                break;
+            case '08003':
+                throw new ServiceUnavailableError(503, 'ERROR: DATABASE SERVER REFUSE THE CONNECTION', error.code);
+                break;
+            case '08000':
+                throw new InternalServerError(500, `ERROR: THE BACKEND CAN'T CONNECT TO THE DATABASE SERVER`, error.code);
+                break;
+            case 'ECONNREFUSED':
+                throw new ServiceUnavailableError(503, `ERROR: THE DATABASE SERVER IS NOT WORKING OR HAVE PERMISSIONS ISSUES`, error.code);
+                break;
+            case 'P0001':
+                throw new NotFoundError(409, `ERROR: DATA NOT FOUND`, error.code);
+                break;
+            default:
+                throw new CustomError(500, 'ERROR: INTERNAL SERVER ERROR', error.code, error.message);
+                break;
+        }
+
+        /*if (error.code === '23505') {
             throw new ConflictError(409, 'ERROR: DUPLICATE KEY VALUE', error.code);
         } else if (error.code === '23503') {
             throw new ConflictError(409, 'ERROR: FOREIGN KEY VIOLATION', error.code);
@@ -37,7 +89,7 @@ class moduleErrorHandler {
             throw new NotFoundError(409, `ERROR: DATA NOT FOUND`, error.code);
         } else {
             throw new CustomError(500, 'ERROR: INTERNAL SERVER ERROR', error.code, error.message);
-        }
+        }*/
     };
 };
 
